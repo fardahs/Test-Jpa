04aef3b94626478c8532cc551b7699acfd21fda6
Farda HOUMADI <farda.houmadi@hotmail.fr>
2019-02-21 16:39:10 +0100

    tp5 relation heritage

--- src/banque/entite/TestJpa.java
+++ src/banque/entite/TestJpa.java
@@ -22,6 +22,8 @@ public class TestJpa {
 		
 		//Liste de clients
 		List<Client> listClient = new ArrayList<Client>();
+		//Liste de comptes
+		List<Compte> listeCompte = new ArrayList<Compte>();
 		
 		Adresse adresse = new Adresse();
 		adresse.setCodePostal(35000);
@@ -37,37 +39,40 @@ public class TestJpa {
 		client.setPrenom("Pierre");
 		client.setBanque(banque);
 		client.setAdresse(adresse);
+		client.setDateNaissance(LocalDate.of(1984, 4, 6));
 		//Ajout d'un client dans la liste	
 		listClient.add(client);
 		
 		banque.setClient(listClient);
 		
-		Operation operation = new Operation();
+		/****************Sans heritage *********************/
+		/*Operation operation = new Operation();
 		operation.setMontant(15487.14);
 		operation.setMotif("Virement de Mr Dupont");
-		operation.setDate(LocalDateTime.now());
+		operation.setDate(LocalDateTime.now());*/
 		
-		Compte compte = new Compte();
+		/*Compte compte = new Compte();
 		compte.setClients(listClient);
 		compte.setNumero("02598745");
-		compte.setSolde(12563.14);
+		compte.setSolde(12563.14);*/
 
 		
 		/*************** TP 5 avec heritage************/
 		LivretA livretA = new LivretA();
 		livretA.setTaux(15.2);
 		livretA.setClients(listClient);
-		livretA.setSolde(14.2);
-		livretA.setNumero("25487");
+		livretA.setSolde(587.4);
+		livretA.setNumero("25487693");
 		
 		Virement virement = new Virement();
 		virement.setBeneficiare("Jacques");
 		virement.setDate(LocalDateTime.now());
-		virement.setCompte(compte);
+		virement.setCompte(livretA);
 		virement.setMontant(254.36);
-		virement.setMotif("Aide");
+		virement.setMotif("Aide 2");
 		
-		operation.setCompte(compte);
+		
+		//operation.setCompte(compte);
 		
 		AssuranceVie assurenceVie = new AssuranceVie();
 		assurenceVie.setClients(listClient);
@@ -76,10 +81,15 @@ public class TestJpa {
 		assurenceVie.setTaux(1.5);
 		assurenceVie.setDateFin(LocalDate.now());
 		
+		listeCompte.add(assurenceVie);
+		listeCompte.add(livretA);
+		
+		client.setComptes(listeCompte);
+		
 		//Persitence des entités
 		em.persist(banque);
-		em.persist(operation);
-		em.persist(compte);
+		//em.persist(operation);
+		//em.persist(compte);
 		em.persist(client);
 		em.persist(livretA);
 		em.persist(virement);
